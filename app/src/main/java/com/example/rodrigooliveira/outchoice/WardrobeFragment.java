a831110a8b71dd185f3f2bbf733984acf0fc04d4
Pedro <sa@student.dei.uc.pt>
2018-11-13 21:53:08 +0000

    Sem Problemas

--- app/src/main/java/com/example/rodrigooliveira/outchoice/WardrobeFragment.java
+++ app/src/main/java/com/example/rodrigooliveira/outchoice/WardrobeFragment.java
@@ -7,16 +7,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.hitomi.cmlibrary.CircleMenu;
-
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class WardrobeFragment extends Fragment {
-
-    String arrayName [] = {"obtjeto 1","obtjeto 2","obtjeto 3"};
-
 
     public WardrobeFragment() {
         // Required empty public constructor
